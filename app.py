abaa6fcbee5947d17135cd2c7879ceba0514883a
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-03-05 20:41:01 -0300

    Aula 05 (Final): Emplementado uma voz para a IA te responder o usuário

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import speech_recognition as sr
 import re
+import pyttsx3
+
 
 mic = sr.Recognizer()
 
 with sr.Microphone() as source:
+    engine = pyttsx3.init()
+    engine.setProperty('voice', 'com.apple.speech.synthesis.voice.luciana')
     mic.adjust_for_ambient_noise(source)
 
     print('Diga alguma coisa...')
@@ -14,11 +18,13 @@ with sr.Microphone() as source:
         frase = mic.recognize_google(audio, language='pt-BR')
 
         if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
-            print('Algo relacionado a ajuda')
+            engine.say('Precisa de alguma ajuda?')
+            engine.runAndWait()
         elif re.search(r'\b' + 'meu nome é' + r'\b', format(frase)):
             t = re.search('meu nome é (.*)', format(frase))
             nome_usuario = t.group(1)
-            print('Olá ' + nome_usuario)
+            engine.say('Olá ' + nome_usuario)
+            engine.runAndWait()
 
     except sr.UnknownValueError:
         print('Desculpe, eu não entendi!')
