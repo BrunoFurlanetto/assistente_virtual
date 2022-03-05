38bd52e866969aece92e653d08bb3df28a8bc95d
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-03-05 20:24:25 -0300

    Aula 04: Reconhecimento da frase 'meu nome é', entendendo ser uma apresentação do usuário

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import speech_recognition as sr
 import re
 
-while True:
-    mic = sr.Recognizer()
+mic = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        mic.adjust_for_ambient_noise(source)
+with sr.Microphone() as source:
+    mic.adjust_for_ambient_noise(source)
 
-        print('Diga alguma coisa...')
+    print('Diga alguma coisa...')
 
-        audio = mic.listen(source)
+    audio = mic.listen(source)
 
-        try:
-            frase = mic.recognize_google(audio, language='pt-BR')
+    try:
+        frase = mic.recognize_google(audio, language='pt-BR')
 
-            if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
-                print('Algo relacionado a ajuda')
+        if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
+            print('Algo relacionado a ajuda')
+        elif re.search(r'\b' + 'meu nome é' + r'\b', format(frase)):
+            t = re.search('meu nome é (.*)', format(frase))
+            nome_usuario = t.group(1)
+            print('Olá ' + nome_usuario)
 
-            print('Você disse: ' + frase)
-
-        except sr.UnknownValueError:
-            print('Desculpe, eu não entendi!')
+    except sr.UnknownValueError:
+        print('Desculpe, eu não entendi!')
