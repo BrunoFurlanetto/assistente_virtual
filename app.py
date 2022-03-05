801e6bbd00d89c5035e933ed7ce5e4774e88a1bf
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-03-05 20:14:45 -0300

    Aula 03: Reconhecimento de palavras chaves e um looping infinito

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import speech_recognition as sr
+import re
 
-mic = sr.Recognizer()
+while True:
+    mic = sr.Recognizer()
 
-with sr.Microphone() as source:
-    mic.adjust_for_ambient_noise(source)
+    with sr.Microphone() as source:
+        mic.adjust_for_ambient_noise(source)
 
-    print('Diga alguma coisa...')
+        print('Diga alguma coisa...')
 
-    audio = mic.listen(source)
+        audio = mic.listen(source)
 
-    try:
-        frase = mic.recognize_google(audio, language='pt-BR')
-        print('Você disse: ' + frase)
-    except sr.UnknownValueError:
-        print('Desculpe, eu não entendi!')
+        try:
+            frase = mic.recognize_google(audio, language='pt-BR')
+
+            if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
+                print('Algo relacionado a ajuda')
+
+            print('Você disse: ' + frase)
+
+        except sr.UnknownValueError:
+            print('Desculpe, eu não entendi!')
