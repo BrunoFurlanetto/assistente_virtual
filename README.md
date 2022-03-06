2034810a3f2809335b44b919b0de584dee079e42
BrunoFurlanetto <bruno.furlanetto@hotmail.com>
2022-03-06 20:05:17 -0300

    Adicionado o vosk para reconhecimento de fala offline

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # Assistente virtual
 
 ### Motivação
-A principal motiação principal para a criação da assistente virtual, 
-<s>ainda sem nome</s>, é principalmente o aprendizado, mas também com aintenção 
-de ficar como portifólio. Dessa forma tomando como base o mini-curso da plataforma 
-da [danki.code](https://cursos.dankicode.com) voltado para dar a base para tal 
-criação.
+<p align="justify">
+A principal motivação para a criação de um assistente virtual,ainda sem nome, é 
+principalmente o aprendizado, mas também com aintenção de ficar como portifólio. 
+Dessa forma tomando como base o mini-curso da plataforma da 
+<a href="https://cursos.dankicode.com">danki.code</a> voltado para dar a base para 
+tal criação.
+</p>
 
 ### Ideias iniciais
+<p align="justify">
 A intenção inicial é fazer a criação de um assitente virtual de voz masculina, 
 com nome ainda a definir, que semelhante aos assistentes mais conhecidos, como 
 Alexa e Siri, irá ficar rodando em segundo plano na máquina e ao ouvir uma palavra 
 chave seguido de seu nome irá se mostrar ouvindo com um som inicial.
+</p>
+
+### Tecnologias
+
+- Reconhecimento de voz:
+  - Online:
+    - Google (SpeechRecognition);
+  - Offline:
+    - Vosk
+
+- Sintese de voz:
+  - pyttsx3
+- Inteligência Artificial
+  - NLU (Para reconhecimento de linguagem natural)
--- app.py
+++ app.py
@@ -1,52 +1,93 @@
+import json
+import time
+from time import sleep
+
 import speech_recognition as sr
 import re
 import pyttsx3
 import os
 import pygame
+import pyaudio
+from vosk import Model, KaldiRecognizer
 
-mic = sr.Recognizer()
-primeiro = True  # Variável pra identificar primeiro acesso do looping
+escutando = True  # Variável pra identificar primeiro acesso do looping
+# --------- Inicialização dos processos -----------
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty("voice", voices[2].id)
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
+stream.start_stream()
+pygame.init()
+pygame.mixer.music.load('escutando.mp3')
+# -------------------------------------------------
+
+
+# Função da fala do assitente
+def falar(frase):
+    engine.say(frase)
+    engine.runAndWait()
+
+
+def escutar():
+    pygame.mixer.music.play()
+    rec = KaldiRecognizer(model, 16000)
+    inicio = time.perf_counter()
+    frase = ''
+
+    while time.perf_counter() - inicio < 4:
+        data = stream.read(2048)
+
+        if len(data) == 0:
+            break
+
+        if rec.AcceptWaveform(data):
+            resultado = rec.Result()
+            resultado = json.loads(resultado)
+
+            if resultado is not None:
+                frase = resultado['text']
+
+    print('Saiu')
+    return frase
+
 
 while True:
-    with sr.Microphone() as source:
-        # --------- Inicialização dos processos -----------
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        engine.setProperty("voice", voices[2].id)
-        mic.adjust_for_ambient_noise(source)
-        # -------------------------------------------------
 
-        if primeiro:
-            pygame.init()
-            pygame.mixer.music.load('escutando.mp3')
-            pygame.mixer.music.play()
-            primeiro = False
+    if escutando:
+        frase = escutar()
+        print(frase)
+        if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
+            falar('Desculpe não posso te ajudar no momento, ainda estou em desenvolvimento')
 
-        audio = mic.listen(source)
+    while True:
+        data = stream.read(2048)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            resultado = rec.Result()
+            resultado = json.loads(resultado)
 
-        try:
-            frase = mic.recognize_google(audio, language='pt-BR')
+            if resultado is not None:
+                frase = resultado['text']
+                print(frase)
 
-            # --------------- Início dos testes para saber o que deverá ser feito -------------------
-            if re.search(r'\b' + 'ajudar' + r'\b', format(frase)):
-                engine.say('Desculpe não posso te ajudar no momento, ainda estou em desenvolvimento')
-                engine.runAndWait()
-                break
+                if 'bruno' in frase:
+                    break
 
-            if re.search(r'\b' + 'meu nome é' + r'\b', format(frase)):
-                t = re.search('meu nome é (.*)', format(frase))
-                nome_usuario = t.group(1)
-                engine.say('Olá ' + nome_usuario + 'é um prazer te conhecer')
-                engine.runAndWait()
-                break
-
-            if re.search(r'\b' + 'abrir o navegador' + r'\b', format(frase)):
-                engine.say('Ok... Abrindo o navegador')
-                engine.runAndWait()
-                os.startfile(r'launcher.exe')
-                break
-            # ---------------------------------------------------------------------------------------
-
-        except sr.UnknownValueError:
-            engine.say('Desculpe eu não entendi, pode repetir?')
-            engine.runAndWait()
+    # try:
+    #     if re.search(r'\b' + 'meu nome é' + r'\b', format(frase)):
+    #         t = re.search('meu nome é (.*)', format(frase))
+    #         nome_usuario = t.group(1)
+    #         engine.say('Olá ' + nome_usuario + 'é um prazer te conhecer')
+    #         engine.runAndWait()
+    #         break
+    #
+    #     if re.search(r'\b' + 'abrir o navegador' + r'\b', format(frase)):
+    #         engine.say('Ok... Abrindo o navegador')
+    #         engine.runAndWait()
+    #         os.startfile(r"C:\Users\bruno\AppData\Local\Programs\Opera\launcher.exe")
+    #         break
+    #     # ---------------------------------------------------------------------------------------
